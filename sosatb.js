5c9f3536c6066f8a328c10c16c2a5411224212b3
Max <filyukhin.m@gmail.com>
2021-06-11 15:10:50 +0300

    vag lol

---
+++ sosatb.js
@@ -0,0 +1,6 @@
+var button = document.querySelector('.btn-buy');
+var text = document.querySelector('.header-title')
+
+button.onclick = function () {
+  text.textContent = 'Японцы делают автомобили, а ВАГ вещи.';
+}
